49a77f120d75a6a4677f16f16d5f819ab82ce63a
JacobLinCool <jacoblincool@gmail.com>
2023-02-11 04:11:33 +0800

    feat: add notice on incomplete feature

--- src/routes/forum/+page.svelte
+++ src/routes/forum/+page.svelte
@@ -2,6 +2,7 @@
 	import { images } from "$lib/images";
 	import { bg } from "$lib/store";
 	import { onMount, onDestroy } from "svelte";
+	import { fly } from "svelte/transition";
 
 	const original_bg = $bg;
 
@@ -25,4 +26,10 @@
 	/>
 </svelte:head>
 
-<section class="pt-12">Forum</section>
+<section class="flex h-full w-full items-center justify-center">
+	<div class="alert bg-base-100 text-sm shadow-lg" transition:fly={{ y: 50, duration: 500 }}>
+		<div>
+			<span> We are still working on this feature. Please check back later. </span>
+		</div>
+	</div>
+</section>
