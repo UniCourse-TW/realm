edab54f0a91c3dd02179c85365fa4a24e278dd01
JacobLinCool <jacoblincool@gmail.com>
2023-02-13 03:35:03 +0800

    fix: ssr title workaround

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,8 @@
 import type { ClientUser } from "$lib/types";
+import { t } from "svelte-i18n";
 import type { LayoutServerLoad } from "./$types";
 
-export const load: LayoutServerLoad = async ({ locals, fetch }) => {
+export const load: LayoutServerLoad = async ({ locals, fetch, route, params }) => {
 	let user: ClientUser | null = null;
 
 	if (locals.crystal) {
@@ -12,7 +13,39 @@ export const load: LayoutServerLoad = async ({ locals, fetch }) => {
 		user = result.data;
 	}
 
+	let title = "UniCourse Realm";
+	let description =
+		"UniCourse Realm is a free, open-source, and community-driven course catalog.";
+
+	if (route.id === "/course/[slug]") {
+		const { slug } = params;
+		const res = await fetch(`/api/course/${slug}`);
+		const course = await res.json();
+		if (course.data) {
+			title = course.data.props.name || "404";
+			description = `UniCourse Realm Course: ${
+				course.data.props.description || course.data.props.name || "Course Not Found"
+			}`;
+		}
+	} else if (route.id === "/course/search") {
+		title = await val("course.search-courses");
+		description = "Find your interested courses on UniCourse";
+	}
+
 	return {
 		user,
+		head: {
+			title,
+			description,
+		},
 	};
 };
+
+function val(key: string): Promise<string> {
+	return new Promise((resolve) => {
+		const unsubscribe = t.subscribe((x) => {
+			resolve(x(key));
+			unsubscribe();
+		});
+	});
+}
--- src/routes/me/+page.svelte
+++ src/routes/me/+page.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
 	import { Role } from "$lib/constants";
-	import type { ClientUser } from "$lib/types";
 	import { t } from "svelte-i18n";
+	import type { PageData } from "./$types";
 	import ImportCoursePack from "./ImportCoursePack.svelte";
 	import Management from "./Management.svelte";
 	import UserInfo from "./UserInfo.svelte";
 	import VerifyEmail from "./VerifyEmail.svelte";
 
-	export let data: {
-		user: ClientUser;
-	};
+	export let data: PageData;
+	const user = data.user!;
 </script>
 
 <svelte:head>
@@ -17,17 +16,17 @@
 </svelte:head>
 
 <section class="py-12">
-	{#if !data.user.roles.includes(Role.Verified)}
-		<VerifyEmail email={data.user.email} />
+	{#if !user.roles.includes(Role.Verified)}
+		<VerifyEmail email={user.email} />
 	{/if}
 
-	<UserInfo user={data.user} />
+	<UserInfo {user} />
 
-	{#if data.user.roles.includes(Role.CoursePacker)}
+	{#if user.roles.includes(Role.CoursePacker)}
 		<ImportCoursePack />
 	{/if}
 
-	{#if data.user.roles.includes(Role.Moderator)}
+	{#if user.roles.includes(Role.Moderator)}
 		<Management />
 	{/if}
 </section>
