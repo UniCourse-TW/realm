e125a1ab017c5a0e83193f3e7b17c78b3c896a70
bogay <pojay11523@gmail.com>
2023-05-01 03:25:02 +0800

    feat: add r2 api

--- src/lib/server/config.ts
+++ src/lib/server/config.ts
@@ -7,3 +7,6 @@ export const NEO4J_USER = process.env.NEO4J_USER || "neo4j";
 export const NEO4J_PASSWORD = process.env.NEO4J_PASSWORD || "password";
 export const JWT_SECRET = process.env.JWT_SECRET || "unicourse-jwt-secret";
 export const FIRST_INVITATION_CODE = process.env.UNICOURSE_FIRST_INVITATION_CODE || "first_code";
+export const CLOUDFLARE_R2_ENDPOINT = process.env.CLOUDFLARE_R2_ENDPOINT;
+export const CLOUDFLARE_ACCESS_KEY_ID = process.env.CLOUDFLARE_ACCESS_KEY_ID;
+export const CLOUDFLARE_SECRET_ACCESS_KEY = process.env.CLOUDFLARE_SECRET_ACCESS_KEY;
---
+++ src/lib/server/r2.ts
@@ -0,0 +1,44 @@
+import { CreateBucketCommand, ListBucketsCommand, S3Client } from "@aws-sdk/client-s3";
+import {
+	CLOUDFLARE_ACCESS_KEY_ID,
+	CLOUDFLARE_R2_ENDPOINT,
+	CLOUDFLARE_SECRET_ACCESS_KEY,
+} from "./config";
+
+export const CLOUDFLARE_R2_BUCKET = "unicourse";
+
+export const ready = (async () => {
+	const client = get_client();
+	const buckets = (await client.send(new ListBucketsCommand({}))).Buckets;
+	if (buckets === undefined) {
+		throw new Error("failed to list r2 buckets");
+	}
+	if (buckets.find((b) => b.Name === CLOUDFLARE_R2_BUCKET) == undefined) {
+		await client.send(
+			new CreateBucketCommand({
+				Bucket: CLOUDFLARE_R2_BUCKET,
+			}),
+		);
+	}
+})();
+
+export function get_client() {
+	if (
+		CLOUDFLARE_R2_ENDPOINT === undefined ||
+		CLOUDFLARE_ACCESS_KEY_ID === undefined ||
+		CLOUDFLARE_SECRET_ACCESS_KEY === undefined
+	) {
+		throw new Error("r2-related config is not set");
+	}
+
+	const s3 = new S3Client({
+		region: "auto",
+		endpoint: CLOUDFLARE_R2_ENDPOINT,
+		credentials: {
+			accessKeyId: CLOUDFLARE_ACCESS_KEY_ID,
+			secretAccessKey: CLOUDFLARE_SECRET_ACCESS_KEY,
+		},
+	});
+
+	return s3;
+}
